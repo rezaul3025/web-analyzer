1cecb3b54a99cfbd5aed2d73ef0d715379ec163a
Md Rezaul Karim <rezaul.km105@gmail.com>
2016-10-28 11:20:22 +0200

    init

--- src/main/java/web/analyzer/service/AnalyzerServiceHandler.java
+++ src/main/java/web/analyzer/service/AnalyzerServiceHandler.java
@@ -1,47 +1,54 @@
 package web.analyzer.service;
 
 import java.io.IOException;
+import java.util.List;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Node;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import web.analyzer.utils.Utils;
 
 @Service
 public class AnalyzerServiceHandler implements AnalyzerService {
 
-	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
 
-	private Connection connection;
+    private Connection connection;
+    
+    @Autowired
+    private Utils utils;
 
-	@Override
-	public AnalyzerService analyze(String url) {
-		// TODO Auto-generated method stub
-		connection = Jsoup.connect(url);
-		try {
-			Document htmlDocument = connection.get();
-			if (connection.response().statusCode() == 200) {
-				
-				System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ : " + htmlDocument.body().text());
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println(e.getLocalizedMessage());
-		}
-		
+    @Override
+    public AnalyzerService analyze(String url) {
+        // TODO Auto-generated method stub
+        connection = Jsoup.connect(url);
+        try {
+            Document htmlDocument = connection.get();
+            if (connection.response().statusCode() == 200) {
 
-		return this;
-	}
+                System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ : " + htmlDocument.body().text());
 
-	@Override
-	public Integer getRequestStatus() {
-		// TODO Auto-generated method stub
-		return connection.response().statusCode();
-	}
+                List<Node> nodes = htmlDocument.childNodes();
+                
+                System.out.println(utils.getDocVersion(nodes));
+             
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println(e.getLocalizedMessage());
+        }
+
+        return this;
+    }
+
+    @Override
+    public Integer getRequestStatus() {
+        // TODO Auto-generated method stub
+        return connection.response().statusCode();
+    }
 
 }
---
+++ src/main/java/web/analyzer/utils/Utils.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.analyzer.utils;
+
+import java.util.List;
+import org.jsoup.nodes.DocumentType;
+import org.jsoup.nodes.Node;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author rkarim
+ */
+@Service
+public class Utils {
+
+    private static final String HTML5_IDENTIFIER = "<!doctype html>";
+
+    private static final String HTML4_VERSION = "4.01";
+    private static final String HTML_IDENTIFIER_STRICT = "strict";
+    private static final String HTML_IDENTIFIER_TRANSITIONAL = "transitional";
+    private static final String HTML_IDENTIFIER_FRAMESET = "frameset";
+
+    private static final String HTML3_VERSION = "3.2";
+
+    private static final String XHTML1_VERSION = "1.0";
+    private static final String XHTML11_VERSION = "1.1";
+   // private static final String XHTM2_VERSION = "2.0";
+
+    public String getDocVersion(List<Node> nodes) {
+        String version = "non";
+        for (Node node : nodes) {
+            if (node instanceof DocumentType) {
+                DocumentType documentType = (DocumentType) node;
+                System.out.println(documentType.toString());
+                String docTypePublicId = documentType.attr("publicid");
+                if (docTypePublicId != null && docTypePublicId.isEmpty()) {
+                    docTypePublicId = docTypePublicId.toLowerCase();
+                    if (docTypePublicId.contains(HTML5_IDENTIFIER)) {
+                        version = "HTML 5";
+                    } else if (docTypePublicId.contains(HTML4_VERSION) && docTypePublicId.contains(HTML_IDENTIFIER_STRICT)) {
+                        version = "HTML 4.01 Strict";
+                    } else if (docTypePublicId.contains(HTML4_VERSION) && docTypePublicId.contains(HTML_IDENTIFIER_TRANSITIONAL)) {
+                        version = "HTML 4.01 Transitional";
+                    } else if (docTypePublicId.contains(HTML4_VERSION) && docTypePublicId.contains(HTML_IDENTIFIER_FRAMESET)) {
+                        version = "HTML 4.01 Frameset";
+                    } else if (docTypePublicId.contains(HTML3_VERSION)) {
+                        version = "HTML 3.2";
+                    } else if (docTypePublicId.contains(XHTML) && docTypePublicId.contains(XHTML1_VERSION) && docTypePublicId.contains(HTML_IDENTIFIER_STRICT)) {
+                        version = "XHTML 1.0 Strict";
+                    } else if (docTypePublicId.contains(XHTML) && docTypePublicId.contains(XHTML1_VERSION) && docTypePublicId.contains(HTML_IDENTIFIER_TRANSITIONAL)) {
+                        version = "XHTML 1.0 Transitional";
+                    } else if (docTypePublicId.contains(XHTML) && docTypePublicId.contains(XHTML1_VERSION) && docTypePublicId.contains(HTML_IDENTIFIER_FRAMESET)) {
+                        version = "XHTML 1.0 Frameset";
+                    } else if (docTypePublicId.contains(XHTML) && docTypePublicId.contains(XHTML11_VERSION)) {
+                        version = "XHTML 1.1";
+                    }
+                }
+                System.out.println(docTypePublicId);
+                return version;
+            }
+        }
+        return version;
+    }
+    private static final String XHTML = "xhtml";
+}
